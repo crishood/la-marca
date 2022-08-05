55741e0dfd9fedeee0470702e7e80f4cd805775c
crishood <crishood13@gmail.com>
2022-08-05 16:09:40 -0500

    Add picture to where section

--- components/Where.js
+++ components/Where.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import guy from "../assets/img/guy.png";
 const Where = () => {
   return (
     <section className="where">
@@ -25,6 +27,26 @@ const Where = () => {
                   magna. Morbi cursus eros commodo erat dignissim sollicitudin.
                   Donec finibus, turpis in pellentesque tempor, justo purus
                   accumsan neque, ut maximus risus nulla vel orci.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="where-picture">
+          <div className="container">
+            <div className="where-picture-description">
+              <div className="where-picture-frame">
+                <Image src={guy} />
+              </div>
+              <div className="where-picture-text">
+                <h2>Aliquam ac felis semper, consequat fidelis. |</h2>
+
+                <p>
+                  Vivamus sit amet posuere nisl, non maximus risus. Maecenas
+                  magna enim, suscipit sit amet ipsum bibendum, semper varius
+                  elit. Integer et tristique eros. Duis vitae pharetra arcu.
+                  Aliquam vel eros tincidunt, vehicula ante sit amet, finibus
+                  magna.
                 </p>
               </div>
             </div>
