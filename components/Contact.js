a4bf8541b6b371856688b6406277f62983c33173
crishood <crishood13@gmail.com>
2022-08-06 13:24:46 -0500

    Create form validations

--- components/Contact.js
+++ components/Contact.js
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const [sent, setSent] = useState(false);
+
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
-
-  console.log(watch("example"));
+  const onSubmit = (data) => {
+    document.getElementById("news-form").reset();
+    setSent(true);
+  };
 
   return (
     <section className="contact" id="contact">
@@ -23,27 +26,89 @@ const Contact = () => {
             fugit.
           </p>
         </div>
-        <form className="contact-form">
+        <form
+          className="contact-form"
+          onSubmit={handleSubmit(onSubmit)}
+          id="news-form"
+        >
           <div className="contact-form-col">
-            <input type="text" placeholder="NOMBRE: " />
-            <select name="select">
-              <option hidden defaultValue>
+            <input
+              type="text"
+              placeholder="NOMBRE: "
+              {...register("name", { required: true })}
+            />
+
+            <select name="select" {...register("docType", { required: true })}>
+              <option disabled selected hidden value="">
                 TIPO DE DOCUMENTO:
               </option>
-              <option value="value1">CÉDULA DE CIUDADANÍA</option>
-              <option value="value2">PASAPORTE</option>
+              <option value="CC">CÉDULA DE CIUDADANÍA</option>
+              <option value="Pasaporte">PASAPORTE</option>
             </select>
-            <input type="text" placeholder="TELÉFONO: " />
+            <input
+              type="text"
+              placeholder="TELÉFONO: "
+              {...register("phone", { required: true, pattern: /^[0-9]*$/ })}
+            />
           </div>
           <div className="contact-form-col">
-            <input type="text" placeholder="CORREO ELECTRÓNICO: " />
-            <input type="text" placeholder="NÚMERO DE DOCUMENTO: " />
+            <input
+              type="text"
+              placeholder="CORREO ELECTRÓNICO: "
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
+            />
+            <input
+              type="text"
+              placeholder="NÚMERO DE DOCUMENTO: "
+              {...register("document", { required: true, pattern: /^[0-9]*$/ })}
+            />
             <div className="contact-form-check">
               <label>
-                <input type="checkbox" /> Acepto términos y condiciones
+                <input
+                  type="checkbox"
+                  {...register("terms", { required: true })}
+                />{" "}
+                Acepto términos y condiciones
               </label>
               <button>SUSCRIBIRME</button>
             </div>
+          </div>
+          <div className="errors">
+            <p>{errors.name?.type === "required" && "* Ingresa tu nombre"}</p>
+            <p>
+              {errors.docType?.type === "required" &&
+                "* Selecciona un tipo de documento"}
+            </p>
+            <p>
+              {errors.phone?.type === "required" ||
+              errors.phone?.type === "pattern"
+                ? "* Ingresa un número de contacto válido"
+                : null}
+            </p>
+
+            <p>
+              {errors.email?.type === "required" ||
+              errors.email?.type === "pattern"
+                ? "* Ingresa un email válido"
+                : null}
+            </p>
+            <p>
+              {errors.document?.type === "required" ||
+              errors.document?.type === "pattern"
+                ? "* Ingresa un número de documento válido"
+                : null}
+            </p>
+            <p>
+              {errors.terms?.type === "required" &&
+                "* Acepta los términos por favor"}
+            </p>
+          </div>
+          <div className="sent">
+            <p>{sent && "Bienvenido/a a nuestro Newsletter"}</p>
           </div>
         </form>
       </div>
